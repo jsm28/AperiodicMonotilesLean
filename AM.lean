e8d2ed890a633a662c82b8ef27a26d6e33985f3a
Joseph Myers <jsm@polyomino.org.uk>
2024-02-20 00:19:01 +0000

    Update mathlib

--- AM.lean
+++ AM.lean
@@ -1,4 +1,2 @@
 import AM.Mathlib.Combinatorics.Tiling.Basic
-import AM.Mathlib.Data.Set.Pointwise.SMul
-import AM.Mathlib.GroupTheory.GroupAction.Basic
 import AM.Mathlib.Logic.Equiv.Pairwise
--- AM/Mathlib/Combinatorics/Tiling/Basic.lean
+++ AM/Mathlib/Combinatorics/Tiling/Basic.lean
@@ -3,9 +3,8 @@ Copyright (c) 2024 Joseph Myers. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Joseph Myers
 -/
-import AM.Mathlib.Data.Set.Pointwise.SMul
-import AM.Mathlib.GroupTheory.GroupAction.Basic
 import AM.Mathlib.Logic.Equiv.Pairwise
+import Mathlib.Algebra.Pointwise.Stabilizer
 import Mathlib.GroupTheory.Coset
 
 /-!
