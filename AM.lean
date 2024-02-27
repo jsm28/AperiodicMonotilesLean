9ee0745d53e0a664e308ef5448f4edc4b68189c1
Joseph Myers <jsm@polyomino.org.uk>
2024-02-27 09:41:59 +0000

    Update mathlib

--- AM.lean
+++ AM.lean
@@ -1,6 +1,5 @@
 import AM.Mathlib.Combinatorics.Tiling.Basic
 import AM.Mathlib.Combinatorics.Tiling.Isohedral
 import AM.Mathlib.Combinatorics.Tiling.Patch
-import AM.Mathlib.Logic.Equiv.Defs
 import AM.Mathlib.Logic.Equiv.Pairwise
 import AM.Mathlib.Logic.Pairwise
