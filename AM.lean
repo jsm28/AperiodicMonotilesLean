b6c379b3bac49d830123277e6be5d358bdc54b44
Joseph Myers <jsm@polyomino.org.uk>
2024-03-05 14:43:11 +0000

    Update mathlib

--- AM.lean
+++ AM.lean
@@ -3,5 +3,4 @@ import AM.Mathlib.Combinatorics.Tiling.Isohedral
 import AM.Mathlib.Combinatorics.Tiling.Patch
 import AM.Mathlib.Combinatorics.Tiling.Periodic
 import AM.Mathlib.Logic.Equiv.Pairwise
-import AM.Mathlib.Logic.Function.Basic
 import AM.Mathlib.Logic.Pairwise
--- AM/Mathlib/Combinatorics/Tiling/Periodic.lean
+++ AM/Mathlib/Combinatorics/Tiling/Periodic.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Joseph Myers
 -/
 import AM.Mathlib.Combinatorics.Tiling.Basic
-import AM.Mathlib.Logic.Function.Basic
 import Mathlib.GroupTheory.OrderOfElement
 
 /-!
