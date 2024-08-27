b3fbb9c1e48784b07f3d0bc8c8846029bcc271bd
Joseph Myers <jsm@polyomino.org.uk>
2024-08-27 01:54:16 +0000

    More towards relating weak and strong periodicity

--- AM.lean
+++ AM.lean
@@ -1,4 +1,5 @@
 import AM.Mathlib.Algebra.Group.Submonoid.Operations
+import AM.Mathlib.Algebra.Module.Submodule.Map
 import AM.Mathlib.Combinatorics.Tiling.Function.Basic
 import AM.Mathlib.Combinatorics.Tiling.Function.Disjoint
 import AM.Mathlib.Combinatorics.Tiling.Function.Tiling
@@ -9,6 +10,8 @@ import AM.Mathlib.Combinatorics.Tiling.Periodic
 import AM.Mathlib.Combinatorics.Tiling.Tile
 import AM.Mathlib.Combinatorics.Tiling.TileSet
 import AM.Mathlib.Data.Setoid.Basic
+import AM.Mathlib.Data.ZMod.Quotient
 import AM.Mathlib.GroupTheory.Coset.Basic
 import AM.Mathlib.GroupTheory.GroupAction.Quotient
 import AM.Mathlib.GroupTheory.Index
+import AM.Mathlib.LinearAlgebra.FreeModule.PIDIndex
