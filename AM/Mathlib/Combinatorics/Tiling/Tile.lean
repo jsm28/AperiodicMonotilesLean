a3b6693ee6d8d7922f6cc961cd62c12ef0266b04
Joseph Myers <jsm@polyomino.org.uk>
2026-01-03 11:04:19 +0000

    Update doc string to suggest `to_additive` for additive groups support
    As suggested in Zulip discussion:
    https://leanprover.zulipchat.com/#narrow/channel/116395-maths/topic/Discussion.20-.20Definition.20of.20tilings/near/566052005

--- AM/Mathlib/Combinatorics/Tiling/Tile.lean
+++ AM/Mathlib/Combinatorics/Tiling/Tile.lean
@@ -39,8 +39,8 @@ group, this is represented by the symmetries specified in the `Prototile` being 
 full stabilizer.
 
 The group `G` is throughout here a multiplicative group. Additive groups are also used in the
-literature, typically when based on `ℤ`; when working with the definitions here, such cases are
-expected to be handled by working with a group such as `Fin n → Multiplicative ℤ`.
+literature, typically when based on `ℤ`; to support the use of additive groups, `to_additive`
+could be used with the theory here.
 
 ## Main definitions
 
